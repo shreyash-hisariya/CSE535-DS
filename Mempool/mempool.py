2deadc012c3c760efe1e0957d6bdec01fa6f8d21
sauver09 <saurabh2612.mnnit@gmail.com>
2021-10-10 17:22:49 -0400

    PythontoClasses

--- Mempool/mempool.py
+++ Mempool/mempool.py
@@ -1,5 +1,13 @@
-import Mempool.intializer as initializer
+#Not needed here
+#validator_info  : consists of all the items which we  were accessing via Main.initializer
+# validator_info {
+#
+# }
 
-def get_transactions():
-    #initializer.transactions.add('Transaction 1')
-    return initializer.transactions
+class Mempool:
+    def __init__(self,transactions,validator_info=None):
+        self.validator_info=validator_info
+        self.transactions = transactions# set()  # this will be filled when a validator is initialized via runner
+    def get_transactions():
+        #initializer.transactions.add('Transaction 1')
+        return self.transactions
--- Pacemaker/pacemaker.py
+++ Pacemaker/pacemaker.py
@@ -1,63 +1,75 @@
-import Pacemaker.initializer as initializer
-from BlockTree.initializer import high_qc
-from Safety.safety import make_timeout
 
-def get_round_timer(r):
-    #distAlgo: set timer for a round expiry
-    pass
 
-def start_timer(new_round):
-    stop_timer(initializer.current_round)
-    initializer.current_round = new_round
-    #distAlgo: start timer for this current_round for duration=get_round_timer(initializer.current_round)
+#validator_info  : consists of all the items which we  were accessing via Main.initializer
+# validator_info {
+#   'BlockTree': BlockTree object
+#   'Safety': Safety object
+# }
 
-def save_consensus_state(self):
-    #doubt
-    pass
+class Pacemaker:
+    def __init__(self,current_round,last_round_tc,pending_timeouts,validator_info=None):
+        self.validator_info=validator_info
+        self.current_round = current_round #0
+        self.last_round_tc = last_round_tc # None
+        self.pending_timeouts = pending_timeouts# {}  # dictionary of set (key:round,value:set of tmo_info  getting timed_out) : may have to verify
 
-def local_timeout_round():
-    save_consensus_state() #need to clarify
-    timeout_info = make_timeout(initializer.current_round, high_qc, initializer.last_round_tc)
 
-    # to do broadCast Timeout_Message()
+    def get_round_timer(self,r):
+        #distAlgo: set timer for a round expiry
+        pass
 
-# To Do timeout
-def process_remote_timeout(tmo):
-    tmo_info = tmo.tmo_info
-    if tmo_info.round < initializer.current_round:
+    def start_timer(self,new_round):
+        self.stop_timer(self.current_round)
+        self.current_round = new_round
+        #distAlgo: start timer for this current_round for duration=get_round_timer(initializer.current_round)
+
+    def save_consensus_state(self):
+        #doubt
+        pass
+
+    def local_timeout_round(self):
+        self.save_consensus_state() #need to clarify
+        timeout_info = self.validator_info["Safety"].make_timeout(self.current_round, self.validator_info["BlockTree"].high_qc, self.last_round_tc)
+
+        # to do broadCast Timeout_Message()
+
+    # To Do timeout
+    def process_remote_timeout(self,tmo):
+        tmo_info = tmo.tmo_info
+        if tmo_info.round < self.current_round:
+            return None
+
+        if tmp_info.sender not in pending_timeouts[tmo_info.round]:
+            pending_timeouts[tmo_info.round].add(tmo_info)
+
+        if len(pending_timeouts[tmo_info.round])==f+1:
+            self.stop_timer(self.current_round)
+            self.local_timeout_round()
+        if len(pending_timeouts[tmo_info.round])==(2*f)+1:
+            high_qc_rounds_vector=[tmo_info.high_qc.vote_info.round for tmo_info in pending_timeouts[tmo_info.round]]
+            signature_list=[tmo_info.signature for tmo_info in pending_timeouts[tmo_info.round]]
+            return TimeoutCertificate(tmo_info.round,high_qc_rounds_vector,signature_list)
+
         return None
+        # if(tmo_info.sender)
 
-    if tmp_info.sender not in pending_timeouts[tmo_info.round]:
-        pending_timeouts[tmo_info.round].add(tmo_info)
-
-    if len(pending_timeouts[tmo_info.round])==f+1:
-        stop_timer(initializer.current_round)
-        local_timeout_round()
-    if len(pending_timeouts[tmo_info.round])==(2*f)+1:
-        high_qc_rounds_vector=[tmo_info.high_qc.vote_info.round for tmo_info in pending_timeouts[tmo_info.round]]
-        signature_list=[tmo_info.signature for tmo_info in pending_timeouts[tmo_info.round]]
-        return TimeoutCertificate(tmo_info.round,high_qc_rounds_vector,signature_list)
-
-    return None
-    # if(tmo_info.sender)
-
-def advance_round_tc(tc):
-    if tc is None or tc.round < initializer.current_round:
-        return False
-    initializer.last_round_tc= tc
-    start_timer(tc.round + 1)
-    return True
+    def advance_round_tc(self,tc):
+        if tc is None or tc.round < self.current_round:
+            return False
+        self.last_round_tc= tc
+        self.start_timer(tc.round + 1)
+        return True
 
 
-def advance_round_qc(qc):
-    if qc.vote_info.round < initializer.current_round:
-        return False
+    def advance_round_qc(self,qc):
+        if qc.vote_info.round < self.current_round:
+            return False
 
-    initializer.last_round_tc = None
-    start_timer(qc.vote_info.round + 1)
-    return True
+        self.last_round_tc = None
+        self.start_timer(qc.vote_info.round + 1)
+        return True
 
 
-def stop_timer(round):
-    # distAlgo:
-    pass
+    def stop_timer(self,round):
+        # distAlgo:
+        pass
